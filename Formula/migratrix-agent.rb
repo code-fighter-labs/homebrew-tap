8ecb733d9f1bc2ec2351eb02fd565ad636973cd3
itai <itaiayanou@gmail.com>
2025-08-19 13:27:51 +0300

    Update migratrix-agent.rb

--- Formula/migratrix-agent.rb
+++ Formula/migratrix-agent.rb
@@ -5,20 +5,20 @@ class MigratrixAgent < Formula
   
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/code-fighter-labs/migratrix/releases/download/agent-v#{version}/migratrix-agent-darwin-arm64.tar.gz"
+      url "https://github.com/code-fighter-labs/homebrew-tap/releases/download/#{version}/migratrix-agent-darwin-arm64.tar.gz"
       sha256 "13bb30769f6b01419fc713faeac89628bf462323e9512e0865137b7191d4109b"
     else
-      url "https://github.com/code-fighter-labs/migratrix/releases/download/agent-v#{version}/migratrix-agent-darwin-amd64.tar.gz"
+      url "https://github.com/code-fighter-labs/homebrew-tap/releases/download/#{version}/migratrix-agent-darwin-amd64.tar.gz"
       sha256 "aa2ca7c421f1e5a4498eae1968ea200e5b2ac535c4e0b81e717f4bb41fb07cbc"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/code-fighter-labs/migratrix/releases/download/agent-v#{version}/migratrix-agent-linux-arm64.tar.gz"
+      url "https://github.com/code-fighter-labs/homebrew-tap/releases/download/#{version}/migratrix-agent-linux-arm64.tar.gz"
       sha256 "1eb1441bf9ffe4eaf33c78e157a4097b90ed6024e4db5dcc1677b82a092919bb"
     else
-      url "https://github.com/code-fighter-labs/migratrix/releases/download/agent-v#{version}/migratrix-agent-linux-amd64.tar.gz"
+      url "https://github.com/code-fighter-labs/homebrew-tap/releases/download/#{version}/migratrix-agent-linux-amd64.tar.gz"
       sha256 "0eff456c84cb5bdd445b4b3e548b548598c35b3aefdbdf61bf2711c1a16d6156"
     end
   end
